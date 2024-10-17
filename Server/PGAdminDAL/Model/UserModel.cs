931bbafdd7a4930721c19131d50de09c66666db2
Dima1385 <Lozik1385@gmail.com>
2024-10-17 22:30:10 +0300

    Update GetUserProfile , add GetUserProfile, add GenerateAdditionalNicknames

--- Server/PGAdminDAL/Model/UserModel.cs
+++ Server/PGAdminDAL/Model/UserModel.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace PGAdminDAL.Model
-{
-    public class UserModel : IdentityUser
+﻿    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    namespace PGAdminDAL.Model
     {
+        public class UserModel : IdentityUser
+        {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Avatar { get; set; }
+        public string? Title { get; set; }
+        public string? Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool EmailConfirmed { get; set; }
+
     }
-}
+    }
