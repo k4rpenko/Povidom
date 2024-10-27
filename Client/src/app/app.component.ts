68823047b8175200631777c76af0350898199894
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-10-27 14:49:24 +0200

    fix side-menu

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuComponent } from "./content/side-menu/side-menu.component";
+import { SideMenuComponent } from "./content/Header/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-root',
--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './content/page-not-found/test.component';
-import { SideMenuComponent } from './content/side-menu/side-menu.component';
+import { PostsComponent } from './content/Main/posts/posts.component';
 
 export const routes: Routes = [
-    { path: '', component: SideMenuComponent },
+    { path: '', component: PostsComponent },
 
     { path: '**', component: PageNotFoundComponent }
  ];
