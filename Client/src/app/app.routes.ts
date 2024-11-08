49c0cfa17f60050e60cf707b527a7521c5b3b541
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2024-11-08 18:09:53 +0200

    Update app.routes.ts

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { NonExistentUserComponent } from './content/Non-existent-user/non-existent-user.component';
-import { PostsComponent } from './content/Main/posts/posts.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MessageComponent } from './pages/message/message.component';
 
 export const routes: Routes = [
-    { path: '', component: PostsComponent },
-    { path: 'Користувач/:username', component: NonExistentUserComponent },
+    { path: '', component: LoginComponent },
+    { path: 'home', component: PostsComponent },
+    { path: 'message', component: MessageComponent },
+    { path: ':username', component: NonExistentUserComponent },
     { path: '**', component: NonExistentUserComponent } 
 ];
