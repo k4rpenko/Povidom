99ad56c7ef8e12c559261f2321781db416ac6dfa
David Karpenko <94958388+k4rpenko@users.noreply.github.com>
2025-01-09 00:06:09 +0200

    add  Generate New UserName

--- Server/Spring_Server/src/main/java/com/Spacegram/server/Models/UserModel.java
+++ Server/Spring_Server/src/main/java/com/Spacegram/server/Models/UserModel.java
@@ -3,4 +3,7 @@ package com.Spacegram.server.Models;
 public class UserModel
 {
     public String id;
+    public String password;
+    public String newPassword;
+    public String nickName;
 }
--- Server/Spring_Server/src/main/java/com/Spacegram/server/controllers/UserSettings.java
+++ Server/Spring_Server/src/main/java/com/Spacegram/server/controllers/UserSettings.java
@@ -1,28 +1,42 @@
 package com.Spacegram.server.controllers;
 
+import com.Spacegram.server.utility.Hash.Sha256;
 import com.Spacegram.server.Models.UserModel;
 import com.Spacegram.server.PostgresDB.Hibernate.HibernateUtil;
 import com.Spacegram.server.PostgresDB.Models.AspNetUsers;
+import com.Spacegram.server.utility.UserServices.userName;
 import org.hibernate.Transaction;
 import org.hibernate.Session;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api")
 public class UserSettings
 {
+    Sha256 _sha256 = new Sha256();
+    userName _userName = new userName();
 
-    @DeleteMapping("/user")
-    public ResponseEntity<String> DeleteUser(@RequestBody UserModel _user){
+    @PutMapping("ChangePassword")
+    public ResponseEntity<String> UpdateUser(@RequestBody UserModel _user)
+    {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             AspNetUsers user = session.get(AspNetUsers.class, _user.id);
             if (user != null) {
-                session.delete(user);
-                transaction.commit();
-                return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+                String HashNewPassword = _sha256.encrypt(user.getConcurrencyStamp(), _user.newPassword);
+                String HashPassword = _sha256.encrypt(user.getConcurrencyStamp(), _user.password);
+
+                if(user.getPasswordHash().equals(HashPassword))
+                {
+                    user.setPasswordHash(HashNewPassword);
+                    session.update(user);
+                    transaction.commit();
+                    return new ResponseEntity<>("User Chang Password", HttpStatus.OK);
+                }
             }
             return new ResponseEntity<>("Not found user ", HttpStatus.NOT_FOUND);
         }
@@ -30,6 +44,27 @@ public class UserSettings
         {
             System.out.println(e);
             return new ResponseEntity<>("Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/NickName")
+    public ResponseEntity<ArrayList<String>> GetUserProfile(@RequestBody UserModel _user)
+    {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            if (session != null) {
+                ArrayList<String> nicknames = new ArrayList<>();
+                for (int i = 0; i < 3; i++){
+                    nicknames.add(_userName.GenerateRandomNickname(_user.nickName, session, nicknames));
+                }
+                return new ResponseEntity<>(nicknames, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
